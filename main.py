c87ed04949d654d327cb143a96ee9cc7959cb046
kristiankaspersen <kristiankasp@gmail.com>
2023-10-08 21:15:54 +0200

    Added a blank character python file which is going to contain character classes, and added some logic for moving a box in the game

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+import sys
+
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+
+# Colors
+WHITE = (255, 255, 255)
+
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Super Mario game")
+
+try:
+    player_img = pygame.image.load("path.png")
+except:
+    player_img = pygame.Surface((40, 60))
+    player_img.fill((255, 0, 0))
+
+player_rect = player_img.get_rect()
+player_speed = 5
+
+clock = pygame.time.Clock()
+running = True
+while running:
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and player_rect.left > 0:
+        player_rect.x -= player_speed
+    if keys[pygame.K_RIGHT] and player_rect.right < WIDTH:
+        player_rect.x += player_speed
+    if keys[pygame.K_UP] and player_rect.top > 0:
+        player_rect.y -= player_speed
+    if keys[pygame.K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect.y += player_speed
+
+    win.fill(WHITE)
+    win.blit(player_img, player_rect.topleft)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
+sys.exit()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
