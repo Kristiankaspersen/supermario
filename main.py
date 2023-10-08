79381474045708c5e6296223dd9bbcb9eac03b21
kristiankaspersen <kristiankasp@gmail.com>
2023-10-08 21:40:35 +0200

    Added gravity and the possibility to jump

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ try:
     player_img = pygame.image.load("path.png")
 except:
     player_img = pygame.Surface((40, 60))
-    player_img.fill((255, 0, 0))
+    player_img.fill((255, 79, 0))
 
 player_rect = player_img.get_rect()
 player_speed = 5
+player_vel_y = 0  # Vertical velocity
+jumping = False   # Jump state
+gravity = 0.5     # Gravity
+jump_height = -15  # Jump velocity
 
 clock = pygame.time.Clock()
 running = True
@@ -31,7 +35,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+
+
     keys = pygame.key.get_pressed()
+    on_ground = player_rect.bottom >= HEIGHT
+
     if keys[pygame.K_LEFT] and player_rect.left > 0:
         player_rect.x -= player_speed
     if keys[pygame.K_RIGHT] and player_rect.right < WIDTH:
@@ -40,6 +48,17 @@ while running:
         player_rect.y -= player_speed
     if keys[pygame.K_DOWN] and player_rect.bottom < HEIGHT:
         player_rect.y += player_speed
+    if keys[pygame.K_SPACE] and not jumping and on_ground:
+        jumping = True
+        player_vel_y = jump_height
+
+    player_vel_y += gravity  # Apply gravity
+    player_rect.y += player_vel_y  # Move player vertically
+
+    if player_rect.bottom >= HEIGHT:
+        player_rect.bottom = HEIGHT
+        jumping = False
+        player_vel_y = 0
 
     win.fill(WHITE)
     win.blit(player_img, player_rect.topleft)
